d3f7523e03034fa664779748397ee04e6ced2e6b
Shahnawaz-Sk <shahnawaz@lambdatest.com>
2024-01-09 18:55:52 +0530

    comments fixes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const logger = require('./src/lib/logger');
 const smartui = require('./src/smartui');
+const pkgName = require('./src/lib/utils').getPackageName()
 
 // Take a DOM snapshot and post it to the snapshot endpoint
 async function smartuiSnapshot(page, snapshotName) {
   if (!page) throw new Error('A Playwright `page` object is required.');
   if (!snapshotName) throw new Error('The `name` argument is required.');
   if (!(await smartui.isSmartUIRunning())) throw new Error('SmartUI server is not running.');
-  let log = logger('@lambdatest-playwrght-sdk');
+  let log = logger(pkgName);
 
   try {
     // Inject the DOM serialization script
--- src/smartui.js
+++ src/smartui.js
@@ -1,6 +1,7 @@
 const client = require('./lib/httpClient.js');
 const logger = require('./lib/logger.js');
-const log = logger('@lambdatest-playwrght-sdk');
+const pkgName = require('./lib/utils.js').getPackageName()
+const log = logger(pkgName);
 
 async function isSmartUIRunning() { 
     try {
